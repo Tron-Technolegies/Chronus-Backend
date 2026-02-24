406be043be28ab7e742f179d926176257cefea39
Parvati <parvathynair186@gmail.com>
2026-02-24 15:42:25 +0530

    'test'

--- ProjectChronas/settings.py
+++ ProjectChronas/settings.py
@@ -172,4 +172,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 STRIPE_SECRET_KEY = env("STRIPE_SECRET_KEY")
 STRIPE_WEBHOOK_SECRET=env("STRIPE_WEBHOOK_SECRET")
+test='test'
 
