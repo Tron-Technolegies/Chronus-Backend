d9c9739f9912b1f447027cb8fadc75a2dfbac254
Parvati <parvathynair186@gmail.com>
2026-02-25 16:29:04 +0530

    'settings-updated'

--- ProjectChronas/settings.py
+++ ProjectChronas/settings.py
@@ -100,12 +100,19 @@ WSGI_APPLICATION = 'ProjectChronas.wsgi.application'
 
 import os
 import cloudinary
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 cloudinary.config(
     cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
     api_key=os.getenv("CLOUDINARY_API_KEY"),
     api_secret=os.getenv("CLOUDINARY_API_SECRET")
 )
+
+
+
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
