5bce427f2c2f6cb5b6cf564b9b36bacd141efda0
Parvati <parvathynair186@gmail.com>
2026-03-10 15:05:13 +0530

    'clear-cart-updated'

--- ChronusUser/urls.py
+++ ChronusUser/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('cart/add/', views.add_to_cart, name='add_to_cart'),
     path('cart/', views.view_cart, name='view_cart'),
     path("cart/remove/<int:product_id>/", views.remove_from_cart, name='remove_from_cart'),
-    path("cart/clear/", views.clear_cart, name= 'clear_cart'),
+    # path("cart/clear/", views.clear_cart, name= 'clear_cart'),
 
     # -------- Wishlist --------
     path('wishlist/add/', views.add_to_wishlist, name='add_to_wishlist'),
